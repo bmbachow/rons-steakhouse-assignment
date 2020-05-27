d424832f7fc6503cdabf03b124eb0022560b96ea
Brian Bachow <bmbachow@gmail.com>
2020-05-26 20:11:04 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Nav from './Nav/Nav'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import HomePage from './HomePage/HomePage'
+import BioPage from './BioPage/BioPage'
+import MenuPage from './MenuPage/MenuPage'
+import NotFoundPage from './NotFoundPage/NotFoundPage'
 import './App.css'
+
 
 export default class App extends Component {
   render() {
@@ -16,7 +21,12 @@ export default class App extends Component {
           <Header />
         </header>
         <main>
-          <HomePage />
+          <Switch>
+           <Route exact path='/' component={HomePage}/>
+           <Route exact path='/bio' component={BioPage}/>
+           <Route exact path='/menu' component={MenuPage}/>
+           <Route component={NotFoundPage}/>
+          </Switch>
         </main>
         <footer>
           <Footer />
